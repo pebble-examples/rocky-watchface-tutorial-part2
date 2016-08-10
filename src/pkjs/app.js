5b458c100f1e54b3970a83ed54b0ccc866c62e8a
Jon Barlow <orviwan@gmail.com>
2016-08-10 22:44:33 +0100

    Final

--- src/pkjs/app.js
+++ src/pkjs/app.js
@@ -1,5 +1,42 @@
-Pebble.addEventListener("ready",
-    function(e) {
-        console.log("Hello world! - Sent from your javascript application.");
-    }
-);
+// PebbleKit JS (pkjs)
+var myAPIKey = '9858eb71a058d377abcd05f64763b42b';
+
+Pebble.on('message', function(event) {
+  // Get the message that was passed
+  var message = event.data;
+
+  if (message.fetch) {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+    	var url = 'http://api.openweathermap.org/data/2.5/weather' +
+              '?lat=' + pos.coords.latitude +
+              '&lon=' + pos.coords.longitude +
+              '&appid=' + myAPIKey;
+      
+			request(url, 'GET', function(respText) {
+			  var weatherData = JSON.parse(respText);
+			  
+			  Pebble.postMessage({
+			    'weather': {
+			      // Convert from Kelvin
+			      'celcius': Math.round(weatherData.main.temp - 273.15),
+			      'fahrenheit': Math.round((weatherData.main.temp - 273.15) * 9 / 5 + 32),
+			      'desc': weatherData.weather[0].main
+			    }
+			  });
+			});
+    }, function(err) {
+      console.error('Error getting location');
+    }, 
+    { timeout: 15000, maximumAge: 60000 });
+  }
+});
+
+function request(url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+}
+
--- src/rocky/index.js
+++ src/rocky/index.js
@@ -1,15 +1,105 @@
-var rocky = _rocky;
+// Rocky.js
+var rocky = require('rocky');
 
-rocky.on('draw', function(drawEvent) {
- var ctx = drawEvent.context;
- var w = parseInt(ctx.canvas.clientWidth);
- var h = parseInt(ctx.canvas.clientHeight);
- var box_side = 20;
+// Global object to store weather data
+var weather;
 
- ctx.fillStyle = 'red';
- ctx.fillRect((w-box_side)/2, (h-box_side)/2, 20, 20);
+rocky.on('hourchange', function(event) {
+  // Send a message to fetch the weather information (on startup and every hour)
+  rocky.postMessage({'fetch': true});
 });
 
-rocky.on('minutechange', function(e) {
- rocky.requestDraw();
+rocky.on('minutechange', function(event) {
+	// Tick every minute
+  rocky.requestDraw();
 });
+
+rocky.on('message', function(event) {
+	// Receive a message from the mobile device (pkjs)
+  var message = event.data;
+
+  if (message.weather) {
+    // Save the weather data
+    weather = message.weather;
+
+    // Request a redraw so we see the information
+    rocky.requestDraw();
+  }
+});
+
+rocky.on('draw', function(event) {
+  var ctx = event.context;
+  var d = new Date();
+
+  // Clear the screen
+  ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+
+  // Draw the conditions (before clock hands, so it's drawn underneath them)
+  if (weather != null) {
+    drawWeather(ctx, weather);
+  }
+
+  // Determine the width and height of the display
+  var w = ctx.canvas.unobstructedWidth;
+  var h = ctx.canvas.unobstructedHeight;
+
+  // Determine the center point of the display
+  // and the max size of watch hands
+  var cx = w / 2;
+  var cy = h / 2;
+
+  // -20 so we're inset 10px on each side
+  var maxLength = (Math.min(w, h) - 20) / 2;
+
+  // Calculate the minute hand angle
+  var minuteFraction = (d.getMinutes()) / 60;
+  var minuteAngle = clockwiseRad(minuteFraction);
+
+  // Draw the minute hand
+  drawHand(ctx, cx, cy, minuteAngle, maxLength, 'white');
+
+  // Calculate the hour hand angle
+  var hourFraction = (d.getHours() % 12 + minuteFraction) / 12;
+  var hourAngle = clockwiseRad(hourFraction);
+
+  // Draw the hour hand
+  drawHand(ctx, cx, cy, hourAngle, maxLength * 0.6, 'lightblue');
+});
+
+function drawWeather(ctx, weather) {
+  // Create a string describing the weather
+  //var weatherString = weather.celcius + 'ºC, ' + weather.desc;
+  var weatherString = weather.fahrenheit + 'ºF, ' + weather.desc;
+
+  // Draw the text, top center
+  ctx.fillStyle = 'lightgray';
+  ctx.textAlign = 'center';
+  ctx.font = '14px Gothic';
+  ctx.fillText(weatherString, ctx.canvas.unobstructedWidth / 2, 2);
+}
+
+function drawHand(ctx, cx, cy, angle, length, color) {
+  // Find the end points
+  var x2 = cx + Math.sin(angle) * length;
+  var y2 = cy + Math.cos(angle) * length;
+
+  // Configure how we want to draw the hand
+  ctx.lineWidth = 8;
+  ctx.strokeStyle = color;
+
+  // Begin drawing
+  ctx.beginPath();
+
+  // Move to the center point, then draw the line
+  ctx.moveTo(cx, cy);
+  ctx.lineTo(x2, y2);
+
+  // Stroke the line (output to display)
+  ctx.stroke();
+}
+
+function clockwiseRad(fraction) {
+  // Convert a fraction into a Radian
+  return (1.5 - fraction) * 2 * Math.PI;
+}
+
